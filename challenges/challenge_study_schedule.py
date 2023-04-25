16ad34553a0f4c68331d0e29361b19c61d61dc5e
Juliana <julianamq20@gmail.com>
2023-04-25 17:20:41 -0300

    project start

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    try:
+        result = [
+            start
+            for start, end in permanence_period
+            if start <= target_time <= end
+        ]
+        return len(result)
+    except TypeError:
+        return None
