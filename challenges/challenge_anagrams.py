bc457c7cac8a735ca6708fcfa9dff4e0f23d636e
Juliana <julianamq20@gmail.com>
2023-04-27 14:01:58 -0300

    tentando desafio 4

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,4 +1,23 @@
+def sorting(string):
+    letter_list = list(string.lower())
+    exchange = True
+    counter = 0
+    while exchange:
+        exchange = False
+        for i in range(len(letter_list) - counter - 1):
+            if letter_list[i] > letter_list[i + 1]:
+                letter_list[i], letter_list[i + 1] = \
+                    letter_list[i + 1], letter_list[i]
+                exchange = True
+        counter += 1
+    return letter_list
+
+
 def is_anagram(first_string, second_string):
-    sorted_first = ''.join(sorted(first_string.lower()))
-    sorted_second = ''.join(sorted(second_string.lower()))
-    return sorted_first == sorted_second
+    sorted_first = sorting(first_string)
+    sorted_second = sorting(second_string)
+    return (
+        ''.join(sorted_first),
+        ''.join(sorted_second),
+        sorted_first == sorted_second
+    )
